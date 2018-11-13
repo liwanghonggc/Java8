2f78b937788fcc8b64480b58d15c56028ce43fd7
lwh <2391519868@qq.com>
2018-11-13 20:25:45 +0800

    Lombok使用Demo

---
+++ src/main/java/com/lwh/java8/chp2/demo1/Apple.java
@@ -0,0 +1,28 @@
+package com.lwh.java8.chp2.demo1;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * @author lwh
+ * @date 2018-11-13
+ * @desp 苹果类
+ *       介绍下Lombok插件,使用Lombok需要安装插件,配置maven依赖,https://blog.csdn.net/motui/article/details/79012846
+ *       lombok依赖其scope为provided,介绍下maven的scope作用域
+ *
+ */
+
+@Data
+@AllArgsConstructor
+public class Apple {
+
+    private String color;
+
+    private int weight;
+
+    public static void main(String[] args) {
+        Apple apple = new Apple("green", 150);
+        System.out.println(apple.toString());
+    }
+}
