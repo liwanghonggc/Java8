ac2ad652cff22d8e26ca7d664ee457125f9da614
lwh <2391519868@qq.com>
2018-11-20 20:32:48 +0800

    Java8学习

--- src/main/java/com/lwh/java8/chp2/demo3/Demo2BuildStream.java
+++ src/main/java/com/lwh/java8/chp2/demo3/Demo2BuildStream.java
@@ -40,6 +40,7 @@ public class Demo2BuildStream {
         Stream.iterate(0, n -> n + 2).limit(10).forEach(System.out::println);
 
         //4.2) 生成
+
         Stream.generate(Math::random).limit(5).forEach(System.out::println);
     }
 }
